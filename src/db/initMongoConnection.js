a541068b642545c09f64a4a71603d22ca74a23da
OL-surgeon <dasti888@ukr.net>
2025-09-15 19:29:39 +0300

    .

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,17 +1,23 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-export const initMongoConnection = async () => {
+dotenv.config();
+
+const initMongoConnection = async () => {
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
+    process.env;
+
+  const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${encodeURIComponent(
+    MONGODB_PASSWORD,
+  )}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
   try {
-    const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
-      process.env;
-
-    const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
-
-    await mongoose.connect(uri);
-
+    await mongoose.connect(MONGODB_URI);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection failed:', error.message);
     process.exit(1);
   }
 };
+
+module.exports = initMongoConnection;

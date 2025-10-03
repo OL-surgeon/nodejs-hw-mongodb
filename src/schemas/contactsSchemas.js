69e9b8e42729329af0076cb4eb632340be671487
OL-surgeon <dasti888@ukr.net>
2025-10-03 18:34:37 +0300

    .

--- src/schemas/contactsSchemas.js
+++ src/schemas/contactsSchemas.js
@@ -2,14 +2,16 @@ import Joi from 'joi';
 
 export const addContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(3).max(20).required(),
-  phone: Joi.string().min(3).max(20).required(),
-  favorite: Joi.boolean(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  email: Joi.string().email().min(3).max(30),
+  isFavourite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  email: Joi.string().email().min(3).max(20),
-  phone: Joi.string().min(3).max(20),
-  favorite: Joi.boolean(),
-}).min(1); // щоб не було пустого body
+  phoneNumber: Joi.string().min(3).max(20),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+  email: Joi.string().email().min(3).max(30),
+  isFavourite: Joi.boolean(),
+}).min(1);

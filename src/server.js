4c1fc55ee3cde190028f7ae7f2c4a2ceb162efa1
OL-surgeon <dasti888@ukr.net>
2025-10-15 20:45:25 +0300

    .

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import { contactsRouter } from './routers/contacts.js';
-import { contactsRout } from './routes/contacts.js';
+
+import router from './routers/index.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { authRouter } from './routers/auth.js';
-dotenv.config();
 
 export const setupServer = () => {
-  const PORT = Number(contactsRout('PORT', '3000'));
+  const PORT = Number(getEnvVar('PORT', '3000'));
   const app = express();
 
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+
   app.use(
     pino({
       transport: {
@@ -27,14 +27,12 @@ export const setupServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
-      message: 'Hello MongoDB!',
+      message: 'Hello MondoDB!',
     });
   });
 
-  app.use('/contacts', contactsRouter);
-  app.use('/auth', authRouter);
+  app.use(router);
   app.use(notFoundHandler);
-
   app.use(errorHandler);
 
   app.listen(PORT, () => {

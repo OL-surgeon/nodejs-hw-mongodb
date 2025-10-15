4ba43ecde8d8d61b448677d1f3c5afe69a0436ff
OL-surgeon <dasti888@ukr.net>
2025-10-15 19:25:46 +0300

    .

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).max(50).required(),
+  password: Joi.string().required(),
 });
 
 export const loginUserSchema = Joi.object({

2778de769d96b74b59d06f8b11f972181bafafc2
OL-surgeon <dasti888@ukr.net>
2025-10-23 19:16:52 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 const startApp = async () => {
   await initMongoConnection();
+  await createDirIfNotExists(TEMP_UPLOAD_DIR);
+  await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
 };
 
 startApp();
-// src/index.js
-
-import { initMongoDB } from './db/initMongoDB.js';
-import { startServer } from './server.js';
-import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
-import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
-
-const bootstrap = async () => {
-  await initMongoDB();
-  await createDirIfNotExists(TEMP_UPLOAD_DIR);
-  await createDirIfNotExists(UPLOAD_DIR);
-  startServer();
-};
-
-void bootstrap();

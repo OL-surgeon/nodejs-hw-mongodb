51b5cf3ca787c8df0475f40b7efab7a92f3d82f2
OL-surgeon <dasti888@ukr.net>
2025-10-27 22:14:57 +0200

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,3 +21,6 @@ export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');

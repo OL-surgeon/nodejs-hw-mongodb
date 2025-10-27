7eaaed3457a01ec2b9727272a05edd46bc296d35
OL-surgeon <dasti888@ukr.net>
2025-10-27 20:58:00 +0200

    .

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,16 +1,22 @@
 import multer from 'multer';
-import path from 'path';
+import path from 'node:path';
+import { TEMP_UPLOAD_DIR } from '../constants/index.js';
 
-const storage = multer.memoryStorage(); // зберігаємо тимчасово в пам’яті
+const storage = multer.diskStorage({
+  destination: TEMP_UPLOAD_DIR,
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
 export const upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname).toLowerCase();
-    if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+    if (!['.jpg', '.jpeg', '.png'].includes(ext)) {
       return cb(new Error('Only images are allowed'));
     }
     cb(null, true);
   },
-  limits: { fileSize: 5 * 1024 * 1024 }, // максимум 5MB
+  limits: { fileSize: 5 * 1024 * 1024 },
 });

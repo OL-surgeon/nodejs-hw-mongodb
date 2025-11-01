f8193c3aa322dfce7b3946d9cf9010c5fd9ce1b2
OL-surgeon <dasti888@ukr.net>
2025-11-01 21:09:58 +0200

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,3 +24,4 @@ export const SORT_ORDER = {
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
